4eb5c190eda7ba9ab22711be851d83c4b6798cd1
aridwia <aridwiananto@gmail.com>
2017-07-09 11:55:46 +0700

    parsingData-1

--- parser.js
+++ parser.js
@@ -1,6 +1,20 @@
 "use strict"
+let fs = require('fs');
 
 class Person {
+
+  constructor(id,first_name,last_name,email,phone,created_at){
+    this.id = id
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.phone = phone
+    this.created_at = new Date().toISOString()
+  }
+
+  get insert(){
+    return `${this.id},${this.first_name},${this.last_name},${this.email},${this.phone},${this.created_at}`
+  }
   // Look at the above CSV file
   // What attributes should a Person object have?
 }
@@ -9,17 +23,54 @@ class PersonParser {
 
   constructor(file) {
     this._file = file
-    this._people = null
+    this._people = [];
   }
 
   get people() {
     return this._people
   }
 
-  addPerson() {}
+  ambil (){
+
+    let dataAwal = fs.readFileSync('people.csv','utf8');
+    let list = dataAwal.trim().split("\n");
+    //let dataL = [];
+
+    for(let i=0;i<list.length;i++){
+      list[i] = list[i].split(",")
+    }
+
+    for(let i=1;i<list.length;i++){
+        let persons = new Person(list[i][0],list[i][1],list[i][2],list[i][3],list[i][4],new Date(list[i][5]))
+          this._people.push(persons)
+    }
+      // let data = list[i];
+      // let dataPer1 = data.split(',')
+      //   dataL.push(dataPer1)
+  return this._people
+  }
+
+  save() {
+   fs.appendFileSync(this._file,this._people + ("\n"),'utf8')
+  }
+
+  addPerson(addData) {
+    this._people.push(addData.insert)
+    return this._people
+  }
+  //
+  // size(){
+  //   return parser.ambil().length
+  // }
 
 }
 
-let parser = new PersonParser('people.csv')
 
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+let parser = new PersonParser('people.csv')
+// let addData = new Person("201","Ar","ri","ari@google.com","898378",(new Date))
+// parser.addPerson(addData)
+// parser.save()
+console.log(parser.ambil());
+// console.log(`There are ${parser.people.length} people in the file '${parser._file}'.`)
+//console.log(parser._people[170 - 1].last_name);
+//console.log(list);
