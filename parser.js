dc5da1d310186ef32afb1828951a82d47e68c508
Adith <adithwidyapradipta@gmail.com>
2017-07-09 22:25:46 +0700

    parser done

--- parser.js
+++ parser.js
@@ -1,25 +1,82 @@
 "use strict"
 
+var fs = require('fs'); //taroh paling atas file. Best practice
+
 class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
-}
+  constructor(id, firstName, lastName, email, phone, createdAt) {
+    this._id = id;
+    this._fName = firstName;
+    this._lName = lastName;
+    this._email = email;
+    this._phone = phone;
 
-class PersonParser {
+    if (createdAt !== undefined) {
+      this.createdAt = (new Date(createdAt)).toISOString();
+    } else {
+      this.createdAt = new Date().toISOString();
+    }
+  }
+}//class
 
+class PersonParser {//Bagian untuk parsing csv jadi data yg bisa diolah pake javascript
   constructor(file) {
-    this._file = file
-    this._people = null
+    this._file = file;
+    this._rawData = this.dataArray();
+    this._people = this.peopleAssigned();
   }
 
   get people() {
-    return this._people
+    return this._people;
   }
 
-  addPerson() {}
+  get file() {
+    return this._file;
+  }
 
-}
+  dataArray() {//Buat csv jadi array
+    let csv = fs.readFileSync(this._file, 'utf-8');
+    let dataArray = csv.split('\n');
+    let arrCsv = [];
+    for (let i = 1; i < dataArray.length; i++) {
+      // dataArray[i] = dataArray[i].split(',');
+      arrCsv.push(dataArray[i].split(','));
+    }
+    return arrCsv;
+  }
+
+  peopleAssigned() {//Buat data array, jadi object array
+    let arrObj = [];
+    let raw = this._rawData;
+
+    for(let i = 1; i < this._rawData.length; i++) {
+      arrObj[i - 1] = new Person(raw[i][0], raw[i][1], raw[i][2], raw[i][3], raw[i][4], raw[i][5]);
+    }
+    return arrObj;
+  }
+
+  addPerson(objectAssigned) {//Nambah data baru ke object array di peopleAssigned()
+    this._people.push(objectAssigned);
+
+    this.save(objectAssigned);
+  }
+
+  save(objectAssigned){//ngesave data yang udah ditambah jadi csv lagi.
+    // let obj = objectAssigned;
+    let string = `\n${objectAssigned._id},${objectAssigned._fName},${objectAssigned._lName},${objectAssigned._email},${objectAssigned._phone},${objectAssigned.createdAt}`;
+    let fs = require('fs');
+    fs.appendFileSync(this._file, string, 'utf-8');
+  }
+
+
+}//class
 
 let parser = new PersonParser('people.csv')
 
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+console.log(`There are ${parser._people.length} people in the file '${parser._file}'.`)
+console.log(parser._people[200])
+
+// parser.addPerson(new Person(201, 'Adith', 'Widya Pradipta', 'adith@gmail.com', '1-790-530-3141'))
+// console.log(`There are ${parser.people.length} people in the file '${parser.file}'.`)
+// console.log(parser.people[202].email)
+
+// console.log(parser.dataArray());
